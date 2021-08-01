0f2597e1332a209f56d5665f8e6a2e121363778c
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2021-08-01 08:48:27 -0400

    fazendo pequenas alteracoes

---
+++ memoria.js
@@ -0,0 +1,11 @@
+// Imagens das cartas
+
+let imagens = [];
+
+for(i = 1, i < 8; i++){
+    imagens.push('http://picsum.photos/id/'+i+'/80');
+};
+
+let fundo = 'http://picsum.photos/80?grayscale';
+
+let eleImagens
